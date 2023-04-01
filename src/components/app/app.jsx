dfad01c3c47666d3ef04bfca4b021d4be0036623
Petr Lipatov <lipatov3@gmail.com>
2023-04-01 15:44:59 +0200

    upd: files infrastructure

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './app.module.css';
+
+class App extends React.Component {
+    render() {
+      return (
+        <div className={ styles.app }>
+        Hello world!
+        </div>
+      )
+    }
+  }
+  
+  export default App
+  
