9d6ab34d5b9e2ea042f9adab4a4e1df1ec919873
Petr Lipatov <lipatov3@gmail.com>
2023-04-02 21:26:31 +0200

    upd: header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Box, Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+import appHeader from './app-header.module.css';
 
+function AppHeader () {
+    return (
+        <header className={appHeader.navContainer}>
+            <nav className={appHeader.navElement}>
+                <div className={appHeader.navContent}>
+                
+                    <div className={appHeader.navSubsection}>
+                        <a className={appHeader.button + " pl-5 pr-5 mb-4 mt-4"}>
+                            <BurgerIcon type="secondary"/>
+                            <span className='text text_type_main-default ml-2'>
+                                Конструктор
+                            </span>
+                        </a>
+                        <a className={appHeader.button + " pl-5 pr-5 mb-4 mt-4"}>
+                            <ListIcon type="secondary"/>
+                            <span className='text text_type_main-default text_color_inactive ml-2'>
+                                Лента заказов
+                            </span>
+                        </a>
+                    </div>
 
-// function AppHeader () {
-//     return (
+                    <div className={`${appHeader.navSubsection} ${appHeader.navLogo}`}>
+                      <Logo/>
+                    </div>
 
-//     )
-// }
+                    <div className={`${appHeader.navSubsection} ${appHeader.navProfile}`}>
+                        <a className={appHeader.button + " pl-5 pr-5 mb-4 mt-4"}>
+                            <ProfileIcon type="secondary"/>
+                            <span className='text text_type_main-default text_color_inactive ml-2'>
+                                Личный кабинет
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </nav>
+            
+        </header>
+    )
+};
+
+export default AppHeader; 
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './app.module.css';
+import AppHeader from '../app-header/app-header.jsx';
+
 
 class App extends React.Component {
     render() {
       return (
         <div className={ styles.app }>
-        Hello world!
+         <AppHeader/>
         </div>
       )
     }
