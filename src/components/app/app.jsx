e66ed7b84c4d3a8afc6cb358696a6937243539e7
Petr <lipatov3@gmail.com>
2023-04-26 21:15:53 +0200

    Revert "Development"

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,41 +1,15 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from 'react';
+import styles from './app.module.css';
 
-import AppHeader from "../app-header/app-header.jsx";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients.jsx";
-import BurgerConstructor from "../burger-constructor/burger-constructor.jsx";
-
-import styles from "./app.module.css";
-import { fetchData } from "../../utils/api.js";
-import { serverAddress } from "../../utils/constants.js";
-
-
-
-function App() {
-  const [data, setData] = useState([]);
-  const [clickedIngredient, setClickedIngredient] = useState(null);
-
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fetchData(serverAddress);
-      setData(data);
-    };
-    getData();
-  }, []);
-
-  return (
-    <div className={styles.page}>
-      <AppHeader />
-      <main className={styles.main}>
-        <BurgerIngredients
-          data={data}
-          clickedIngredient={clickedIngredient}
-          setClickedIngredient={setClickedIngredient}
-        />
-        <BurgerConstructor data={data} />
-      </main>
-    </div>
-  );
-}
-
-export default App;
+class App extends React.Component {
+    render() {
+      return (
+        <div className={ styles.app }>
+        Hello world!
+        </div>
+      )
+    }
+  }
+  
+  export default App
+  
